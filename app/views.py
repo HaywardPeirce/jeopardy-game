9311b45e317f98b9d4642f8361a44c9eac1e272f
Hayward Peirce <HaywardPeirce@users.noreply.github.com>
2017-03-10 13:21:12 -0800

    HP: adding game page view

---
+++ app/views.py
@@ -0,0 +1,23 @@
+# views.py
+
+from flask import render_template
+from flask import request
+from app import app
+
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# Now you can import your module
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/game')
+def game():
+
+    return render_template('game.html')
--- question.py
+++ question.py
@@ -9,9 +9,25 @@ config.read('config.ini')
 questionFile = config['GENERAL']['questionFile']
 
 #a python dictionary of all the questions
-questionList = readInQuestions(questionFile)
+questionList = readInQuestions()
+
+questionListLen = len(questionList)
 
 #todo create object for player and game to keep track of stats, questions, and scores
+class Game:
+    # Point class for the game being played.
+
+    def __init__(self):
+        self.turn = 0
+        self.players = 0
+        self.pickedQuestions = []
+
+class Player:
+    # Point class for the player of a game.
+
+    def __init__(self):
+        self.score = 0
+
 
 #function that keeps track of the game
 def game():
@@ -20,13 +36,13 @@ def game():
 def newGame():
     print('hello world')
 
-#function called when a new turn is started
+#function called when a new turn is started, returns the question to be asked
 def getNewQuestion():
-    nextQuestion = pickQuestion()
+    nextQuestionIndex = pickQuestion()
 
     #select the next question based on it's index
-    question = questionList['questions'][nextQuestion]
-    game.pickedQuestions.append(nextQuestion)
+    question = questionList[nextQuestionIndex]
+    game.pickedQuestions.append(nextQuestionIndex)
 
     return question
 
@@ -35,12 +51,19 @@ def pickQuestion(exclude):
     r = None
     #loop until the random number that is picked is not a question that has laready been selected.
     while r in exclude or r is None:
-         r = random.randrange(1,10)
+         r = random.randrange(0,(questionListLen-1))
     return r
 
-#return a python dictionary of
+#return a python list of questions
 def readInQuestions():
     with open(questionFile) as json_data:
         questionData = json.load(json_data)
-        print(type(questionData))
-        print(questionData[1])
+        #print(type(questionData))
+        #print(questionData[1])
+    return questionData
+
+def main():
+    time.sleep(delay)
+
+if __name__ == '__main__':
+    main()
